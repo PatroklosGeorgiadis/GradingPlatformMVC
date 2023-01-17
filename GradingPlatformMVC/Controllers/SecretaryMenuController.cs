ae91a5b1397b37e4ac0fbe8fbb4e29e11a17a3af
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-01-18 01:27:53 +0200

    4th attempt

---
+++ GradingPlatformMVC/Controllers/SecretaryMenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GradingPlatformMVC.Controllers
+{
+    public class SecretaryMenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

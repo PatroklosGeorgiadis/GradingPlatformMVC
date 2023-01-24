53e4093e9021ed1caf6b8d6ee1391d8fcbc1123e
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-01-24 21:26:58 +0200

    course title fix (test)

--- GradingPlatformMVC/Controllers/CourseHasStudentsController.cs
+++ GradingPlatformMVC/Controllers/CourseHasStudentsController.cs
@@ -32,7 +32,7 @@ namespace GradingPlatformMVC.Controllers
             }
 
             int PageSize = 10;
-            var gradesData = grades.ToPagedList(page ?? 1, PageSize);
+            var gradesData = grades.Include(e => e.IdCourseNavigation).ToPagedList(page ?? 1, PageSize);
 
             return View(gradesData);
         }

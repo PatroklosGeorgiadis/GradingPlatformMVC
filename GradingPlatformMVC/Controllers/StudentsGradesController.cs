8de41098abc789847cc9354ec9be8eb6f2983c3c
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-01-26 00:27:37 +0200

    idiot moment

--- GradingPlatformMVC/Controllers/StudentsGradesController.cs
+++ GradingPlatformMVC/Controllers/StudentsGradesController.cs
@@ -111,7 +111,7 @@ namespace GradingPlatformMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdCourse,RegistrationNum,Grade,IdCourse,RegistrationNum")] CourseHasStudent courseHasStudent)
+        public async Task<IActionResult> Create([Bind("IdCourse,RegistrationNum,Grade")] CourseHasStudent courseHasStudent)
         {
             if (ModelState.IsValid)
             {
